f31d90dce90e88a3e564359e96ca5f61a978aa21
R Jacqueline Young <jacqueline@rjyoungenterprises.com>
2018-08-19 08:18:46 -0500

    Deploy new updates

---
+++ wp-content/themes/accelerate-theme-child/case-study_code.php
@@ -0,0 +1,11 @@
+<aside class+ "case-study-sidebar">
+<h2>Mailchimp Campaign</h2>
+<h4>Content Strategy, Design & Development</h4>
+<h4>Client: Mailchimp</h4>
+</aside>
+
+<div class="case-study-images">
+<img src="http://localhost/accelerate/wp-content/uploads/2018/07/mc-thumbnail-1.png" alt="" width="606" height="446" class="alignnone size-full wp-image-47342" />
+<img src="http://localhost/accelerate/wp-content/uploads/2018/07/mc-thumbnail-2.png" alt="" width="606" height="817" class="alignnone size-full wp-image-47343" />
+<img src="http://localhost/accelerate/wp-content/uploads/2018/07/mc-thumbnail-3.png" alt="" width="606" height="1211" class="alignnone size-full wp-image-47344" />
+</div>
